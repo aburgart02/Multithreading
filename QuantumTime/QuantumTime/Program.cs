0f5ededf4d1b99b9b8280845453df141d685e007
Артём <a.burgart02@gmail.com>
2022-11-01 19:05:13 +0500

    Add QuantumTime

---
+++ QuantumTime/QuantumTime/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Task
+{
+    class Program
+    {
+        public static void Payload()
+        {
+            var s = "";
+            for (int i = 0; i < 100; i++)
+                s += "a";
+        }
+
+        public static void Main()
+        {
+            var process = Process.GetCurrentProcess();
+            var data = new List<Tuple<int, long>>();
+            process.ProcessorAffinity = (IntPtr)Math.Pow(2, (Environment.ProcessorCount - 1));
+            process.PriorityClass = ProcessPriorityClass.RealTime;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Thread thread1 = new Thread(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    lock (data)
+                    {
+                        Payload();
+                        data.Add(Tuple.Create(1, stopwatch.ElapsedMilliseconds));
+                    }
+                }
+            });
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    lock (data)
+                    {
+                        Payload();
+                        data.Add(Tuple.Create(2, stopwatch.ElapsedMilliseconds));
+                    }
+                }
+            });
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            GetQuantum(data);
+        }
+
+        public static void GetQuantum(List<Tuple<int, long>>  data)
+        {
+            int count = 0;
+            long sum = 0;
+            long time = 0;
+            int threadNumber = data[0].Item1;
+            foreach (var item in data)
+            {
+                if (item.Item1 != threadNumber)
+                {
+                    count++;
+                    sum += (item.Item2 - time);
+                    threadNumber = item.Item1;
+                    time = item.Item2;
+                }
+            }
+            Console.WriteLine(sum / count);
+        }
+    }
+}
