b616ab872cc04fb0b913e454354f4b538915bfe1
Артём <a.burgart02@gmail.com>
2022-11-14 19:01:35 +0500

    Fix

--- IMultiLock/IMultiLock/Program.cs
+++ IMultiLock/IMultiLock/Program.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System;
 using System.Collections.Generic;
-
+using System.Collections.Concurrent;
 
 namespace Task
 {
@@ -10,15 +10,121 @@ namespace Task
     {
         public static void Main()
         {
+            void Test1()
+            {
+                var testVariable = new[] { "1", "2", "3" };
+                var testVariable2 = new[] { "1" };
+                var testVariable3 = new[] { "2", "3" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable))
+                    {
+                        Thread.Sleep(1000);
+                    }
 
+                });
+                var thread2 = new Thread(() =>
+                {
+                    Thread.Sleep(500);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+                        Thread.Sleep(500);
+                        using (multilock.AcquireLock(testVariable3))
+                        {
+
+                        }
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+            }
+
+            void Test2()
+            {
+                var testVariable = new[] { "b", "c" };
+                var testVariable2 = new[] { "c", "b" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable))
+                    {
+
+                    }
+
+                });
+                var thread2 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+            }
+
+            void Test3()
+            {
+                var testVariable = new[] { "a" };
+                var testVariable2 = new[] { "b" };
+                var testVariable3 = new[] { "c" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+
+                });
+                var thread2 = new Thread(() =>
+                {
+                    Thread.Sleep(500);
+                    using (multilock.AcquireLock(testVariable3))
+                    {
+
+                    }
+                });
+                var thread3 = new Thread(() =>
+                {
+                    Thread.Sleep(1000);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+                        using (multilock.AcquireLock(testVariable3))
+                        {
+
+                        }
+                    }
+                });
+                var thread4 = new Thread(() =>
+                {
+                    Thread.Sleep(3000);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+                thread4.Start();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                Test1();
+                Test2();
+                Test3();
+            }
         }
     }
 
     public class Disposable : IDisposable
     {
-        private readonly StringWrapper[] blockedObjects;
+        private readonly object[] blockedObjects;
 
-        public Disposable(IEnumerable<StringWrapper> blockedObjects) =>
+        public Disposable(object[] blockedObjects) =>
             this.blockedObjects = blockedObjects.ToArray();
 
         public void Dispose()
@@ -28,6 +134,7 @@ namespace Task
                 if (Monitor.IsEntered(blockedObject))
                     Monitor.Exit(blockedObject);
             }
+            Console.WriteLine("Объект разблокирован");
         }
     }
 
@@ -38,43 +145,31 @@ namespace Task
 
     public class MultiLock : IMultiLock
     {
-        private readonly Dictionary<string, StringWrapper> stringWrappers = new Dictionary<string, StringWrapper>();
+        private readonly static ConcurrentDictionary<string, object> dictionary = new ConcurrentDictionary<string, object>();
 
         public IDisposable AcquireLock(params string[] keys)
         {
-            lock (stringWrappers)
+            object syncObj = new object();
+            lock (syncObj)
             {
-                foreach (var key in keys)
-                    if (!stringWrappers.ContainsKey(key))
-                        stringWrappers[key] = new StringWrapper(key);
-            }
+                lock (dictionary)
+                {
+                    foreach (var key in keys)
+                        dictionary.GetOrAdd(key, syncObj);
+                }
 
-            var blockedObjects = keys
-                .Select(i => stringWrappers[i])
-                .OrderBy(i => i.String)
-                .ToArray();
+                var blockedObjects = keys
+                    .Select(i => dictionary[i])
+                    .OrderBy(i => i)
+                    .ToArray();
 
-            var disposable = new Disposable(blockedObjects);
+                var disposable = new Disposable(blockedObjects);
 
-            var isError = true;
-            try
-            {
                 foreach (var blockedObject in blockedObjects)
                     Monitor.Enter(blockedObject);
-                isError = false;
+                Console.WriteLine("Объект заблокирован");
+                return disposable;
             }
-            finally
-            {
-                if (isError)
-                    disposable.Dispose();
-            }
-            return disposable;
         }
-    }
-
-    public class StringWrapper
-    {
-        public string String { get; }
-        public StringWrapper(string @string) => String = @string;
     }
 }
