79d06c0e06441cd87b0925a4a0466765ac5bf459
Артём <a.burgart02@gmail.com>
2022-11-14 19:59:36 +0500

    Fix

--- IMultiLock/IMultiLock/Program.cs
+++ IMultiLock/IMultiLock/Program.cs
@@ -8,9 +8,163 @@ namespace Task
 {
     class Program
     {
-        public static void Main()
-        {
+        private static IMultiLock _multiLock2 = new MultiLock();
 
+        public static void Main(string[] args)
+        {
+            void Test0()
+            {
+                var t1 = new Thread(x => UseResource(TimeSpan.FromSeconds(10), "1", "3"));
+                var t2 = new Thread(x => UseResource(TimeSpan.FromSeconds(2), "4", "3"));
+                var t3 = new Thread(x => UseResource(TimeSpan.FromSeconds(1), "4", "5"));
+                t1.Name = "1";
+                t2.Name = "2";
+                t3.Name = "3";
+                t1.Start();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                t2.Start();
+                t3.Start();
+                t1.Join();
+                t2.Join();
+                t2.Join();
+            }
+
+            void Test1()
+            {
+                var testVariable = new[] { "1", "2", "3" };
+                var testVariable2 = new[] { "1" };
+                var testVariable3 = new[] { "2", "3" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable))
+                    {
+                        Thread.Sleep(1000);
+                    }
+
+                });
+                var thread2 = new Thread(() =>
+                {
+                    Thread.Sleep(500);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+                        Thread.Sleep(500);
+                        using (multilock.AcquireLock(testVariable3))
+                        {
+
+                        }
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+            }
+
+            void Test2()
+            {
+                var testVariable = new[] { "b", "c" };
+                var testVariable2 = new[] { "c", "b" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable))
+                    {
+
+                    }
+
+                });
+                var thread2 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+            }
+
+            void Test3()
+            {
+                var testVariable = new[] { "a" };
+                var testVariable2 = new[] { "b" };
+                var testVariable3 = new[] { "c" };
+                var multilock = new MultiLock();
+                var thread1 = new Thread(() =>
+                {
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+
+                });
+                var thread2 = new Thread(() =>
+                {
+                    Thread.Sleep(500);
+                    using (multilock.AcquireLock(testVariable3))
+                    {
+
+                    }
+                });
+                var thread3 = new Thread(() =>
+                {
+                    Thread.Sleep(1000);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+                        using (multilock.AcquireLock(testVariable3))
+                        {
+
+                        }
+                    }
+                });
+                var thread4 = new Thread(() =>
+                {
+                    Thread.Sleep(3000);
+                    using (multilock.AcquireLock(testVariable2))
+                    {
+
+                    }
+                });
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+                thread4.Start();
+            }
+
+            Test0();
+            for (var i = 0; i < 10; i++)
+            {
+                Test1();
+                Test2();
+                Test3();
+            }
+        }
+
+
+        private static void UseResource(TimeSpan fromSeconds, params string[] keys)
+        {
+            // Wait until it is safe to enter.
+            Console.WriteLine("{0} is requesting the mutex",
+                Thread.CurrentThread.Name);
+            var _lock = _multiLock2.AcquireLock(keys);
+            //var m = new Mutex(false, "1");
+            //m.WaitOne();
+            Console.WriteLine("{0} has entered the protected area",
+                Thread.CurrentThread.Name);
+
+            // Place code to access non-reentrant resources here.
+
+            // Simulate some work.
+            Thread.Sleep(fromSeconds);
+
+            Console.WriteLine("{0} is leaving the protected area",
+                Thread.CurrentThread.Name);
+
+            // Release the Mutex.
+            _lock.Dispose();
+            //m.ReleaseMutex();
+
+            Console.WriteLine("{0} has released the mutex",
+                Thread.CurrentThread.Name);
         }
     }
 
@@ -28,6 +182,7 @@ namespace Task
                 if (Monitor.IsEntered(blockedObject))
                     Monitor.Exit(blockedObject);
             }
+            Console.WriteLine("Объект разблокирован");
         }
     }
 
@@ -56,18 +211,10 @@ namespace Task
 
             var disposable = new Disposable(blockedObjects);
 
-            var isError = true;
-            try
-            {
-                foreach (var blockedObject in blockedObjects)
-                    Monitor.Enter(blockedObject);
-                isError = false;
-            }
-            finally
-            {
-                if (isError)
-                    disposable.Dispose();
-            }
+            foreach (var blockedObject in blockedObjects)
+                Monitor.Enter(blockedObject);
+            Console.WriteLine("Объект заблокирован");
+
             return disposable;
         }
     }
